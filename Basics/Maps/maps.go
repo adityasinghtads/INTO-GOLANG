959ed1eb87762d2ccfbd51ef69c725074862a86e
Aditya Singh <thakur.11913421@lpu.in>
2022-07-05 23:55:25 +0530

    MAPS

---
+++ Basics/Maps/maps.go
@@ -0,0 +1,68 @@
+package main
+
+import "fmt"
+
+func main() {
+	mapIntro()
+	mapUsingMake()
+	updateMap()
+	retriveMapData()
+}
+
+func mapIntro() {
+	var map1 map[int]int
+	if map1 == nil {
+		fmt.Println("True")
+	} else {
+		fmt.Println("False")
+	}
+	map2 := map[int]string{
+		10: "dog",
+		11: "cat",
+		12: "Adi",
+		13: "singh",
+	}
+	fmt.Println("Map1:", map2)
+}
+
+func mapUsingMake() {
+	var mmap = make(map[int]int)
+	fmt.Println(mmap)
+	mmap[1] = 1
+	mmap[2] = 2
+	fmt.Println(mmap)
+
+	// iterating in MAP
+	for id, value := range mmap {
+		fmt.Println(id, value)
+	}
+}
+
+func updateMap() {
+	my_map := map[int]string{
+		1: "adi",
+		2: "singh",
+		3: "Aditya",
+		4: "Birds",
+	}
+	fmt.Println("Original Map", my_map)
+	//update
+	my_map[1] = "parrot"
+	my_map[2] = "hi"
+	//add
+	my_map[8] = "eight"
+	my_map[9] = "nine"
+
+	fmt.Println("Maps after Updation", my_map)
+}
+
+func retriveMapData() {
+	var mymap map[int]string
+	mymap[1] = "one"
+	mymap[2] = "two"
+	mymap[3] = "three"
+	mymap[4] = "four"
+
+	value1 := mymap
+	fmt.Println(value1)
+}

0424a4609fa5d1033abf556d2250cc7f974c2913
Aditya Singh <thakur.11913421@lpu.in>
2022-07-11 23:40:48 +0530

    Structure with Golang - 11 July 22

--- Basics/structs/struct.go
+++ Basics/structs/struct.go
@@ -5,7 +5,12 @@ import "fmt"
 func main() {
 	structIntro()
 	accessUsingDot()
+	pointerToStuct()
+	nestedStruct()
+	ananymonusStruct()
 }
+
+// ⁡⁣⁢⁣************** Structure Intro *************⁡
 
 func structIntro() {
 	type Address struct {
@@ -32,6 +37,8 @@ func structIntro() {
 
 }
 
+// ⁡⁣⁢⁣************ Struct access using Dot operator *****************⁡
+
 func accessUsingDot() {
 	type Address struct {
 		name    string
@@ -40,5 +47,61 @@ func accessUsingDot() {
 		state   string
 		pincode int
 	}
+	a := Address{name: "Vedanta", street: 23}
+	println(a)
 
+	fmt.Println("Name:", a.name)
+	fmt.Println("Street", a.street)
+}
+
+// ⁡⁣⁢⁣************ Poiner to Struct *****************⁡
+
+func pointerToStuct() {
+	type Address struct {
+		name    string
+		street  int
+		city    string
+		state   string
+		pincode int
+	}
+
+	emp1 := &Address{name: "Aditya Singh"}
+	fmt.Println("Name is ", (*emp1).name)
+	// Other way to define this is
+	fmt.Println("name is ", emp1.name)
+	// Checking out other things as well
+	fmt.Println("Complete", emp1)
+
+}
+
+// ⁡⁣⁢⁣************ Nested Structure *****************⁡
+
+func nestedStruct() {
+	type subject struct {
+		name  string
+		hours int
+	}
+	type teacher struct {
+		name    string
+		age     int
+		details subject
+	}
+
+	Manya := teacher{"Manya", 25, subject{"science", 25}}
+	fmt.Println(Manya)
+
+	Divya := teacher{name: "divya", age: 26, details: subject{"maths", 26}}
+	fmt.Println(Divya)
+}
+
+func ananymonusStruct() {
+	// rule for anonymous Struct is the value for the struct should be defined immedatiely afte the struct.
+	aditya := struct {
+		name string
+		age  int
+	}{
+		name: "TADS",
+		age:  22,
+	}
+	fmt.Println(aditya)
 }

96e73813768f9274b03fcf6a63860e7c4c91b751
Aditya Singh <thakur.11913421@lpu.in>
2022-07-12 22:34:47 +0530

    Types of functions - 12-Jul-22

--- Basics/functions/functions.go
+++ Basics/functions/functions.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	add(1, 2)
@@ -8,6 +11,13 @@ func main() {
 	b := 9
 	callByRefrence(&a, &b)
 	callByValue(5, 6)
+
+	// ************* Anonymous function *************
+
+	func(element int) {
+		fmt.Println(element)
+	}(8)
+
 }
 
 func add(a, b int) int {
@@ -39,4 +49,33 @@ func callByRefrence(a, b *int) int {
 	*b = *a
 	*b = c
 	return c
+}
+
+// *************** Varadic Function **************
+// to pass zero or multiple arguments in the function the number or the variables names dont need to be provided.
+
+func varadicFunc(element ...string) string {
+	return strings.Join(element, "-")
+}
+
+// *************** Init Function ****************
+// The min purpse of init function is to run before main function in Golang, apart from this there can be multiple init functions in a program.
+
+func init() {
+	fmt.Println("Into Init function")
+}
+
+// ************ Underscore Identifier in Golang ************
+
+func Underscore() {
+	mulDiv(5, 5) // returns the correct value
+
+	a, b := mulDiv(6, 6)
+	println(a, b)
+
+	_, c := mulDiv(7, 7)
+	println(c)
+}
+func mulDiv(a, b int) (int, int) {
+	return a * b, a / b
 }

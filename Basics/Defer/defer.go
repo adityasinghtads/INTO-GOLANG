a4f5f1be30215e6b1ef166c295f451cde14c85b3
Aditya Singh <thakur.11913421@lpu.in>
2022-07-13 22:38:16 +0530

    Defer in Golang 13-Jul-22

---
+++ Basics/Defer/defer.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// ********** Defer statements *********
+// defer is the keyword which means the code or lne will run at the end of the program after all others are done.
+
+func mul(a, b int) int {
+	return a + b
+}
+
+func main() {
+	fmt.Println("start")
+	//defer mul(3, 2)
+	defer fmt.Println(mul(3, 2))
+	fmt.Println(mul(2, 2))
+	//mul(2, 2)
+	fmt.Println("End")
+}

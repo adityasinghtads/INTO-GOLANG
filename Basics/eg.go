7fbe5cf6117117378ffbfdc62545e2ce23921b1d
Aditya Singh <thakur.11913421@lpu.in>
2022-07-16 23:37:38 +0530

    16july22

--- Basics/eg.go
+++ Basics/eg.go
@@ -2,18 +2,121 @@ package main
 
 import "fmt"
 
-func main() {
-	var f int
-	fmt.Scanln(&f)
-	//your code goes here
-	switch {
-	case f < 0:
-		fmt.Println("Wrong Input")
-	case f > 20000:
-		fmt.Println("Ultrasound")
-	case f < 20:
-		fmt.Println("Infrasound")
-	default:
-		fmt.Println("Audible")
-	}
+type Coord struct {
+	x int
+	y int
 }
+
+func main() {
+	a := Coord{7, 5}
+	fmt.Println(a.x - a.y)
+}
+
+// ********** Salary using struct ***********
+
+// type Employee struct {
+// 	name     string
+// 	age      int
+// 	position string
+// 	salary   float32
+// }
+
+// func main() {
+// 	e1 := Employee{"James", 42, "Manager", 0}
+
+// 	var x float32
+// 	fmt.Scanln(&x)
+// 	e1.salary = x
+
+// 	fmt.Println("========")
+// 	fmt.Println(e1.name + "(" + e1.position + ")")
+// 	fmt.Println(e1.age)
+// 	fmt.Println(e1.salary)
+// 	fmt.Println("========")
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var num float32
+// 	var factor float32
+
+// 	fmt.Scanln(&num)
+// 	fmt.Scanln(&factor)
+
+// 	scale(&num, factor)
+// 	fmt.Println(num)
+// }
+// func scale(a *float32, b float32) {
+// 	*a = *a * b
+// }
+
+// //************** pointes general *******
+// // package main
+
+// // import "fmt"
+
+// // func main() {
+// // 	var x int
+// // 	fmt.Scanln(&x)
+// // 	fmt.Println(&x)
+// // }
+
+// // ******** Pointers *******
+// // func main() {
+// // 	a := 4
+// // 	p := &a
+// // 	a += 2
+// // 	*p = *p - 1
+// // 	fmt.Println(a)
+// // }
+// // func mars_age(a int) int {
+// // 	daysOnEarth := a * 365
+// // 	yearsOnMars := daysOnEarth / 687
+// // 	return yearsOnMars
+// // }
+
+// // *********  MARS AGE *************
+// // func main() {
+// // 	var age int
+// // 	fmt.Scanln(&age)
+
+// // 	mars := mars_age(age)
+// // 	fmt.Println(mars)
+// // }
+// // func mars_age(a int) int {
+// // 	daysOnEarth := a * 365
+// // 	yearsOnMars := daysOnEarth / 687
+// // 	return yearsOnMars
+// // }
+
+// // package main
+
+// // import "fmt"
+
+// // func double(a int) {
+// // 	fmt.Println(a * 2)
+// // }
+// // func main() {
+// // 	for i := 4; i > 0; i-- {
+// // 		defer double(i)
+// // 	}
+// // }
+
+// // func main() {
+// // 	var f int
+// // 	fmt.Scanln(&f)
+// // 	//your code goes here
+// // 	switch {
+// // 	case f < 0:
+// // 		fmt.Println("Wrong Input")
+// // 	case f > 20000:
+// // 		fmt.Println("Ultrasound")
+// // 	case f < 20:
+// // 		fmt.Println("Infrasound")
+// // 	default:
+// // 		fmt.Println("Audible")
+// // 	}
+// // }
---
+++ Basics/robot.go
@@ -0,0 +1,80 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c int
+	fmt.Scanln(&a)
+	fmt.Scanln(&b)
+	fmt.Scanln(&c)
+	if a == 0 {
+		fmt.Println("Zero")
+	} else if a == 1 {
+		fmt.Println("One")
+	} else if a == 2 {
+		fmt.Println("Two")
+	} else if a == 3 {
+		fmt.Println("Three")
+	} else if a == 4 {
+		fmt.Println("Four")
+	} else if a == 5 {
+		fmt.Println("Five")
+	} else if a == 6 {
+		fmt.Println("Six")
+	} else if a == 7 {
+		fmt.Println("Seven")
+	} else if a == 8 {
+		fmt.Println("Eight")
+	} else if a == 9 {
+		fmt.Println("Nine")
+	} else if a == 10 {
+		fmt.Println("Ten")
+	}
+	if b == 0 {
+		fmt.Println("Zero")
+	} else if b == 1 {
+		fmt.Println("One")
+	} else if b == 2 {
+		fmt.Println("Two")
+	} else if b == 3 {
+		fmt.Println("Three")
+	} else if b == 4 {
+		fmt.Println("Four")
+	} else if b == 5 {
+		fmt.Println("Five")
+	} else if b == 6 {
+		fmt.Println("Six")
+	} else if b == 7 {
+		fmt.Println("Seven")
+	} else if b == 8 {
+		fmt.Println("Eight")
+	} else if b == 9 {
+		fmt.Println("Nine")
+	} else if b == 10 {
+		fmt.Println("Ten")
+	}
+	if c == 0 {
+		fmt.Println("Zero")
+	} else if c == 1 {
+		fmt.Println("One")
+	} else if c == 2 {
+		fmt.Println("Two")
+	} else if c == 3 {
+		fmt.Println("Three")
+	} else if c == 4 {
+		fmt.Println("Four")
+	} else if c == 5 {
+		fmt.Println("Five")
+	} else if c == 6 {
+		fmt.Println("Six")
+	} else if c == 7 {
+		fmt.Println("Seven")
+	} else if c == 8 {
+		fmt.Println("Eight")
+	} else if c == 9 {
+		fmt.Println("Nine")
+	} else if c == 10 {
+		fmt.Println("Ten")
+	}
+
+}

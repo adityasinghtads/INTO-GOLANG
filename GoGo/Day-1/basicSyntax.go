d8973dbed973498b11d854301b09f8157191108f
Thakur Aditya Dhanraj Singh <aditya.dhanraj@internal.utradesolutions.com>
2023-02-03 17:50:09 +0530

    First commit in New Branch

---
+++ GoGo/Day-1/basicSyntax.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a bool = true
+	fmt.Print(a)
+	allDatatypes()
+}
+
+func allDatatypes() {
+	var a bool = true
+	var b int = 12
+	var c string = "Aditya "
+	var d float32 = 2.12
+
+	// To print  the above state ments
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+
+}
